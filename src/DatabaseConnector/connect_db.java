8dd5165dfa0f988103304cc2b910639349f70521
bonbybao169 <bonby201@gmail.com>
2021-12-13 23:22:31 +0700

    add gitignore

--- src/DatabaseConnector/connect_db.java
+++ src/DatabaseConnector/connect_db.java
@@ -1,14 +1,8 @@
 package DatabaseConnector;
 
 import java.sql.Connection;
-import java.sql.Date;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
