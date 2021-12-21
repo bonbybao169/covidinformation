bfa66c4c83ee407bf63e4b1330ed72a5d7886d9a
bonbybao169 <bonby201@gmail.com>
2021-12-22 02:55:53 +0700

    update buyEP

--- src/Patient/Database/BuyEssentialPackage.java
+++ src/Patient/Database/BuyEssentialPackage.java
@@ -11,20 +11,18 @@ import java.sql.Date;
 public class BuyEssentialPackage {
 
     String sql;
-    ResultSet rs;
     Connection conn = createConnection();
     PreparedStatement psm = null;
 
     public void buyEssentialPackagesByID(Float newDebt, String mpID) {
-        EssentialPackage temp = null;
-        Date date;
 
         try {
             sql = "UPDATE mp_infor SET Debt = ? WHERE CCCD = ?";
             psm = conn.prepareStatement(sql);
             psm.setFloat(1, newDebt);
             psm.setString(2, mpID);
-            rs = psm.executeQuery();
+            System.out.println("Im in buy bf ex");
+            psm.executeUpdate();
             conn.close();
             psm.close();
         } catch (SQLException e) {
