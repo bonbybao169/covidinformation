1efbff74456a85142371cc18c28a126ace35352e
TuongVy0910 <63916775+TuongVy0910@users.noreply.github.com>
2022-01-08 21:09:36 +0700

    add payment history

---
+++ src/Patient/Database/UpdatePaymentHistory.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Patient.Database;
+
+import static DatabaseConnector.connect_db.createConnection;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author HOME
+ */
+public class UpdatePaymentHistory {
+
+    String sql;
+    Connection conn = createConnection();
+    PreparedStatement psm = null;
+
+    public void updateConsumptionHistory(String mpID, int money) {
+        Date date;
+
+        try {
+            sql = "insert into consumption_history(MPID,Content,Cash,Time) values (?,?,?,?)";
+            psm = conn.prepareStatement(sql);
+            psm.setString(1, mpID);
+            psm.setString(2, "Thanh toán ngày" + java.time.LocalDate.now());
+            psm.setInt(3, money);
+            date = Date.valueOf(java.time.LocalDate.now());
+            psm.setDate(4, date);
+            int executeUpdate = psm.executeUpdate();
+            conn.close();
+            psm.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
