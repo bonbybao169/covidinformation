71f59cbaf4f456d93d432d7893993781e232b423
TuongVy0910 <63916775+TuongVy0910@users.noreply.github.com>
2022-01-09 22:20:49 +0700

    update manager controller

--- src/Manager/Controller/ManagerController.java
+++ src/Manager/Controller/ManagerController.java
@@ -4,6 +4,7 @@
  */
 package Manager.Controller;
 
+import Manager.Service.AddressDetail;
 import Manager.Service.CovidManagementService;
 import Manager.Service.EssentialPackageManagementService;
 import Manager.Service.StatisticService;
@@ -17,96 +18,96 @@ import java.util.ArrayList;
  * @author HOME
  */
 public class ManagerController {
-
+    
     public ArrayList<Patient> view_patient_list() {
         ArrayList<Patient> list = new ArrayList<>();
         CovidManagementService service = new CovidManagementService();
         list = service.showList();
         return list;
     }
-
+    
     public ArrayList<Patient> sort_ASC_byName() {
-
+        
         CovidManagementService service = new CovidManagementService();
-
+        
         return service.sortASCByName();
     }
-
+    
     public ArrayList<Patient> sort_DESC_byName() {
-
+        
         CovidManagementService service = new CovidManagementService();
-
+        
         return service.sortDESCByName();
     }
-
+    
     public ArrayList<Patient> sort_ASC_byHopitalName() {
-
+        
         CovidManagementService service = new CovidManagementService();
-
+        
         return service.sortASCByHopital();
     }
-
+    
     public ArrayList<Patient> sort_DESC_byHopitalName() {
-
+        
         CovidManagementService service = new CovidManagementService();
-
+        
         return service.sortDESCByHopital();
     }
-
+    
     public ArrayList<Patient> sort_ASC_byState() {
-
+        
         CovidManagementService service = new CovidManagementService();
-
+        
         return service.sortASCByState();
     }
-
+    
     public ArrayList<Patient> sort_DESC_byState() {
-
+        
         CovidManagementService service = new CovidManagementService();
-
+        
         return service.sortDESCByState();
     }
-
+    
     public ArrayList<Patient> sort_ASC_byDebt() {
-
+        
         CovidManagementService service = new CovidManagementService();
-
+        
         return service.sortASCByDebt();
     }
-
+    
     public ArrayList<Patient> sort_DESC_byDebt() {
-
+        
         CovidManagementService service = new CovidManagementService();
-
+        
         return service.sortDESCByDebt();
     }
-
+    
     public ArrayList<Patient> sort_ASC_byDOB() {
-
+        
         CovidManagementService service = new CovidManagementService();
-
+        
         return service.sortASCByDOB();
     }
-
+    
     public ArrayList<Patient> sort_DESC_byDOB() {
-
+        
         CovidManagementService service = new CovidManagementService();
-
+        
         return service.sortDESCByDOB();
     }
-
+    
     public void transfer_State(String mnId, String CCCD, int oldState, int newState) {
         CovidManagementService service = new CovidManagementService();
         service.transferStatus(CCCD, newState);
         service.addTransferStatusHistoryForManager(mnId, CCCD, Integer.toString(oldState), Integer.toString(newState));
     }
-
+    
     public void transfer_Hopital(String mnId, String CCCD, String oldHopital, String newHopitalID, String newHopitalName) {
         CovidManagementService service = new CovidManagementService();
         service.transferIsolationArea(CCCD, newHopitalID);
         service.addTransferIsolationAreaHistoryForManager(mnId, CCCD, oldHopital, newHopitalName);
     }
-
+    
     public int add_newPatient(String mnId, Patient newMP) {
         CovidManagementService service = new CovidManagementService();
         if (service.findByCCCD(newMP.getCCCD()) != null) {
@@ -121,62 +122,62 @@ public class ManagerController {
         service.addNewMPAccountHistoryForManager(mnId, newMP.getCCCD());
         return 1;
     }
-
+    
     public ArrayList<String[]> view_Isolation_Area_List() {
         CovidManagementService service = new CovidManagementService();
         return service.findIsolationArea();
     }
-
+    
     public ArrayList<String[]> view_RelatedPeople_List(String CCCD) {
         CovidManagementService service = new CovidManagementService();
         return service.findRelatedPeopleList(CCCD);
     }
-
+    
     public Patient searchPatient(String MPID) {
         CovidManagementService service = new CovidManagementService();
         return service.findByCCCD(MPID);
     }
-
+    
     public void add_NewEssentialPackage(String mnId, String epID, String name, int limitPeople, Date limitDate, float price) {
         EssentialPackageManagementService service = new EssentialPackageManagementService();
         service.addNewEssentialPackage(epID, name, limitPeople, limitDate, price);
         service.addNewEPHistoryForManager(mnId, epID);
-
+        
     }
-
+    
     public void UpdateEssentialPackage(String mnId, String epID, String name, int limitPeople, Date limitDate, float price) {
         EssentialPackageManagementService service = new EssentialPackageManagementService();
         service.updateEssentialPackage(epID, name, limitPeople, limitDate, price);
         service.addUpdateEPHistoryForManager(mnId, epID);
-
+        
     }
-
+    
     public void DelEssentialPackage(String mnId, String epID) {
         EssentialPackageManagementService service = new EssentialPackageManagementService();
         service.deleteEssentialPackage(epID);
         service.addDelEPHistoryForManager(mnId, epID);
-
+        
     }
-
+    
     public ArrayList<String[]> view_Consumption_Statisitc(String mnId) {
         StatisticService service = new StatisticService();
         service.addStatistictHistoryForManager(mnId, "tiêu thụ");
         return service.showConsumption();
-
+        
     }
-
+    
     public ArrayList<String[]> view_State_Statisitc(String mnId) {
         StatisticService service = new StatisticService();
         service.addStatistictHistoryForManager(mnId, "số lượng người theo từng trạng thái");
         return service.showStateStatistic();
     }
-
+    
     public ArrayList<String[]> view_Cured_Statisitc(String mnId) {
         StatisticService service = new StatisticService();
         service.addStatistictHistoryForManager(mnId, "F0 được chữa khỏi");
         return service.showCuredStatistic();
     }
-
+    
     public ArrayList<String[]> view_Debt_Statisitc(String mnId) {
         StatisticService service = new StatisticService();
         CovidManagementService s = new CovidManagementService();
@@ -190,24 +191,39 @@ public class ManagerController {
         service.addStatistictHistoryForManager(mnId, "dư nợ");
         return list;
     }
-
+    
     public String cal_total_Debt() {
         StatisticService service = new StatisticService();
         return Float.toString(service.calTotalDebt());
     }
-
+    
     public String cal_total_Cured() {
         StatisticService service = new StatisticService();
         return Integer.toString(service.calTotalCured());
     }
-
+    
     public String cal_total_Consumption() {
         StatisticService service = new StatisticService();
         return Integer.toString(service.calTotalConsumption());
     }
-
+    
     public String cal_total_Patient() {
         StatisticService service = new StatisticService();
         return Integer.toString(service.calTotalPatient());
+    }
+    
+    public ArrayList<String> getCity() {
+        AddressDetail service = new AddressDetail();        
+        return service.getCityList();
+    }
+    
+    public ArrayList<String> getDistrictofCity(String cName) {
+        AddressDetail service = new AddressDetail();        
+        return service.getDistrictByCityName(cName);
+    }
+    
+    public ArrayList<String> getWardofDistrict(String dName) {
+        AddressDetail service = new AddressDetail();        
+        return service.getWardtByDistrictName(dName);
     }
 }
